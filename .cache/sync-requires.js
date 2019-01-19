15b349c8aee3d935b3cecaf06a6f4161809f19aa
William <wla@g.hmc.edu>
2019-01-19 00:43:25 -0800

    trying to fix deploy

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -3,7 +3,7 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---node-modules-gatsby-plugin-offline-app-shell-js": preferDefault(require("C:\\Users\\2000w\\williamLa\\node_modules\\gatsby-plugin-offline\\app-shell.js")),
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\2000w\\williamLa\\.cache\\dev-404-page.js")),
   "component---src-pages-index-js": preferDefault(require("C:\\Users\\2000w\\williamLa\\src\\pages\\index.js"))
 }
 
