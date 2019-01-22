83100c9e8de1fc8394d9041a199f195f559064ed
William <wla@g.hmc.edu>
2019-01-22 12:54:07 -0800

    deleted cache files

--- .cache/sync-requires.js
+++
@@ -1,9 +0,0 @@
-// prefer default export if available
-const preferDefault = m => m && m.default || m
-
-
-exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\2000w\\williamLa\\.cache\\dev-404-page.js")),
-  "component---src-pages-index-js": preferDefault(require("C:\\Users\\2000w\\williamLa\\src\\pages\\index.js"))
-}
-
